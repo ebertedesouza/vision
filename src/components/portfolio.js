9ab86daeb065b10263401cd61f4d898b187c19b3
Eberte <ebertesouza16@gmail.com>
2026-01-31 21:47:22 -0300

    1.3

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,18 +3,34 @@ export function createPortfolio() {
   section.classList.add('portfolio');
   section.id = 'portfolio';
 
+  const images = [
+    '/img/portifolio/serralheria-01.jpg',
+    '/img/portifolio/serralheria-02.jpg',
+    '/img/portifolio/serralheria-03.jpg',
+
+    '/img/portifolio/cobertura-01.jpg',
+    '/img/portifolio/cobertura-02.jpg',
+    '/img/portifolio/cobertura-03.jpg',
+
+    '/img/portifolio/eletrica-01.jpg',
+    '/img/portifolio/eletrica-02.jpg',
+    '/img/portifolio/eletrica-03.jpg',
+  ];
+
   section.innerHTML = `
     <div class="container">
       <h2>Portfólio</h2>
+
       <div class="portfolio-gallery">
-        <img src="img/portfolio/trabalho1.jpg" alt="Trabalho 1">
-        <img src="img/portfolio/trabalho2.jpg" alt="Trabalho 2">
-        <img src="img/portfolio/trabalho3.jpg" alt="Trabalho 3">
-        <img src="img/portfolio/trabalho4.jpg" alt="Trabalho 4">
+        ${images
+          .map(
+            (src) =>
+              `<img src="${src}" alt="Serviço realizado pela Vision" loading="lazy">`
+          )
+          .join('')}
       </div>
     </div>
   `;
 
   return section;
 }
-
