89aef3b8e34d6f371178a3bb94f91457bf7405f8
Eberte <ebertesouza16@gmail.com>
2026-02-01 15:16:32 -0300

    1.9

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,23 +4,23 @@ export function createPortfolio() {
   section.id = 'portfolio';
 
   const images = [
-    '/img/portifolio/serralheria-01.jpg',
-    '/img/portifolio/serralheria-02.jpg',
-    '/img/portifolio/serralheria-03.jpg',
-
-    '/img/portifolio/cobertura-01.jpg',
-    '/img/portifolio/cobertura-02.jpg',
-    '/img/portifolio/cobertura-03.jpg',
-
-    '/img/portifolio/eletrica-01.jpg',
-    '/img/portifolio/eletrica-02.jpg',
-    '/img/portifolio/eletrica-03.jpg',
+    '/img/portfolio/serralheria-01.jpg',
+    '/img/portfolio/serralheria-02.jpg',
+    '/img/portfolio/serralheria-03.jpg',
+    '/img/portfolio/cobertura-01.jpg',
+    '/img/portfolio/cobertura-02.jpg',
+    '/img/portfolio/cobertura-03.jpg',
+    '/img/portfolio/eletrica-01.jpg',
+    '/img/portfolio/eletrica-02.jpg',
+    '/img/portfolio/eletrica-03.jpg',
+    '/img/portfolio/hidraulica-1.jpg',
+    '/img/portfolio/hidraulica-2.jpg',
+    '/img/portfolio/hidraulica-3.jpg',
   ];
 
   section.innerHTML = `
     <div class="container">
       <h2>Portfólio</h2>
-
       <div class="portfolio-gallery">
         ${images
           .map(
